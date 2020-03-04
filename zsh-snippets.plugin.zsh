c32d00274d84fce16281ddcf23d6dc9cde9375a3
Ilan Cosman <ilancosman@gmail.com>
2020-03-03 21:31:28 -0800

    Initial commit

---
+++ zsh-snippets.plugin.zsh
@@ -0,0 +1,52 @@
+# Install custom snippet system
+typeset -Ag abbreviations
+abbreviations=()
+
+just_alias() {
+  alias $1="$2"
+}
+
+just_expansion() {
+  if [[ "$2" == *\^* ]]
+  then
+    abbreviations[$1]="$2"
+  else
+    abbreviations[$1]="$2 ^"
+  fi
+}
+
+snippet() {
+  just_alias $1 $2
+  just_expansion $1 $2
+}
+
+als() {
+  snippet $1 $2
+  echo "snippet '$1' '$2'" >> ~/.dotfiles/zsh_snippets.zsh
+}
+
+source ~/.dotfiles/zsh_snippets.zsh
+
+magic-abbrev-expand() {
+    local MATCH
+    TEMP="$LBUFFER"
+    SNIPPET=${abbreviations[$LBUFFER]}
+
+    if [[ -n "$SNIPPET" ]]
+    then
+      LBUFFER=${SNIPPET[(ws:^:)1]}
+      RBUFFER=${SNIPPET[(ws:^:)2]}
+    else
+      zle self-insert
+    fi
+
+}
+
+no-magic-abbrev-expand() {
+  LBUFFER+=' '
+}
+
+zle -N magic-abbrev-expand
+zle -N no-magic-abbrev-expand
+bindkey " " magic-abbrev-expand
+bindkey "^x " no-magic-abbrev-expand
