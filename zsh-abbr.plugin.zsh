e4ee2fe62dc0787aef85488c2f54c621a28fd37e
IlanCosman <ilancosman@gmail.com>
2020-03-16 11:33:18 -0700

    Unify expand functions

--- zsh-abbr.plugin.zsh
+++ zsh-abbr.plugin.zsh
@@ -1,49 +1,40 @@
 declare -A expandableMap # Initialize an associative array called expandableMap
 
 abbr() {
-  arg1=${1[(ws:=:)1]}
-  arg2=${1[(ws:=:)2]}
-
+  local arg1=${1[(ws:=:)1]}
+  local arg2=${1[(ws:=:)2]}
+  
   expandableMap[$arg1]="$arg2"
 }
 
-spaceExpand() {
+expand() {
   expandable=${expandableMap[$LBUFFER]}
-
-  if [[ -n "$expandable" ]] # If expandable is not an empty string
-  then
-    if [[ "$expandable" == *"^"* ]] #If expandable contains a ^
-    then
+  
+  if [[ -n "$expandable" ]] ; then # If expandable is not an empty string
+    if [[ "$expandable" == *"^"* ]] ; then # If expandable contains a ^
       LBUFFER=${expandable[(ws:^:)1]} # Then split expandable around the first ^
       RBUFFER=${expandable[(ws:^:)2]} # and set the R/L buffers equal to the two parts
     else
-      LBUFFER="$expandable " #If no ^, just set the left buffer equal to expandable
+      LBUFFER="$expandable " # If no ^, just set the left buffer equal to expandable
+      if [[ "$1" == "Enter" ]] ; then # If passed "Enter", accept the line
+        zle accept-line
+      fi
     fi
-  else
-    zle self-insert # If expandable is an empty string, just send a normal keypress   
+  else # If expandable is an empty string
+    if [[ "$1" == "Enter" ]] ; then # If passed "Enter", accept the line
+      zle accept-line
+    else
+      zle self-insert #Otherwise, send a normal keypress
+    fi
   fi
 }
 
 enterExpand() {
-  expandable=${expandableMap[$LBUFFER]}
-
-  if [[ -n "$expandable" ]]
-  then
-    if [[ "$expandable" == *"^"* ]]
-    then
-      LBUFFER=${expandable[(ws:^:)1]}
-      RBUFFER=${expandable[(ws:^:)2]}
-    else
-      LBUFFER="$expandable "
-      zle accept-line
-    fi
-  else
-    zle accept-line 
-  fi
+  expand "Enter"
 }
 
-zle -N spaceExpand
+zle -N expand
 zle -N enterExpand
 
-bindkey " " spaceExpand
+bindkey " " expand
 bindkey "^M" enterExpand
